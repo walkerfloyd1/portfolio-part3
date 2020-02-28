4d26908453a36d551c67ed744406790cebd82cd4
Walker Floyd <walkerkeatley@yahoo.com>
2020-02-28 13:01:41 -0800

    added screenwriting page component

---
+++ src/Pages/Screenwriting.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <div class="container">
+                        <h1>
+                            Screenwriting
+                        </h1>
+                </div>
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default Profile;
